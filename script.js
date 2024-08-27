474f395a847c7a597d70548cd2118aeef6374658
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-27 14:09:31 +0500

    add the js with objects

---
+++ script.js
@@ -0,0 +1,16 @@
+const a = {
+    name: "fest",
+    location: { city: "Jizzax" },
+    lat: 1233.32,
+    lon: 302.32,
+    someone: "age",
+  };
+  
+  const b = {
+    name: "test",
+    location: { city: "Samarqand" },
+    lat: 1233.32,
+    lon: 302.32,
+    someone: "age",
+  };
+  
